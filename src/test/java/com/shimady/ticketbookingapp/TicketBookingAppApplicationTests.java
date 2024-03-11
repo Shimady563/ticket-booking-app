8fd189c33809350eeab9f630bed23f115268f502
Maksim Shiryaev <shimady563@gmail.com>
2024-03-11 21:03:59 +0300

    Initial commit

---
+++ src/test/java/com/shimady/ticketbookingapp/TicketBookingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shimady.ticketbookingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketBookingAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
