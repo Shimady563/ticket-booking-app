880a0d2133b55a15352b67fc976c380a7a94a314
Shimady563 <shimady563@gmail.com>
2024-03-28 18:21:41 +0300

    feat(model): create entities for user and booking

---
+++ src/main/java/com/shimady/ticketbookingapp/model/User.java
@@ -0,0 +1,41 @@
+package com.shimady.ticketbookingapp.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.Set;
+
+@NoArgsConstructor
+@Getter
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "full_name")
+    private String fullName;
+
+    @Column(name = "email")
+    private String email;
+    
+    @Column(name = "password")
+    private String password;
+
+    @OneToMany(mappedBy = "user", fetch = FetchType.LAZY)
+    private Set<Booking> bookings;
+
+    public void addBooking(Booking booking) {
+        bookings.add(booking);
+        booking.setUser(this);
+    }
+
+    public void removeBooking(Booking booking) {
+        bookings.remove(booking);
+        booking.setUser(null);
+    }
+}
