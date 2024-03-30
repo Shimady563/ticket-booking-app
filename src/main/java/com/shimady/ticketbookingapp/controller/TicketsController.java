262ed7d100518231a028317927894e7325b26d4e
Maksim Shiryaev <shimady563@gmail.com>
2024-03-30 18:25:08 +0300

    feat(tickets): create controller for viewing one way tickets

---
+++ src/main/java/com/shimady/ticketbookingapp/controller/TicketsController.java
@@ -0,0 +1,74 @@
+package com.shimady.ticketbookingapp.controller;
+
+import com.shimady.ticketbookingapp.service.TicketsService;
+import com.shimady.ticketbookingapp.model.Flight;
+import com.shimady.ticketbookingapp.model.SeatType;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.time.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+@RequestMapping("/search")
+public class TicketsController {
+
+    private final TicketsService ticketsService;
+
+    @Autowired
+    public TicketsController(TicketsService ticketsService) {
+        this.ticketsService = ticketsService;
+    }
+
+    @PostMapping("/")
+    private List<TicketsResponse> getSeatsInfo(@RequestBody TicketsRequest request) {
+        List<Flight> flights = ticketsService.getFlights(
+                request.sourceAirportCode,
+                request.destinationAirportCode,
+                request.departureTime
+        );
+
+        List<TicketsResponse> ticketsResponses = new ArrayList<>();
+        for (Flight flight : flights) {
+            ticketsService.getSeatByType(flight, request.seatType()).ifPresent(
+                    seat -> ticketsResponses
+                            .add(new TicketsResponse(
+                                    flight.getId(),
+                                    seat.getPrice(),
+                                    request.seatType,
+                                    flight.getDepartureTime(),
+                                    flight.getArrivalTime(),
+                                    flight.getSourceAirport().getCity(),
+                                    flight.getDestinationAirport().getCity(),
+                                    ticketsService.getEstimatedTime(flight.getDepartureTime(), flight.getArrivalTime())
+                            )));
+
+        }
+
+        return ticketsResponses;
+    }
+
+    private record TicketsRequest(
+            String sourceAirportCode,
+            String destinationAirportCode,
+            LocalDate departureTime,
+            SeatType seatType
+    ) {
+    }
+
+    private record TicketsResponse(
+            Long flightId,
+            int seatPrice,
+            SeatType seatType,
+            LocalDateTime departureTime,
+            LocalDateTime arrivalTime,
+            String departureCity,
+            String arrivalCity,
+            LocalTime estimatedTime
+    ) {
+    }
+}
