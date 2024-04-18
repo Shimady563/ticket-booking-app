446edb6b4d5a99bb1dc7360e29a0371b97830fac
Maksim Shiryaev <shimady563@gmail.com>
2024-04-18 11:21:20 +0300

    feat(tickets): add missing things to seats related classes add transaction management to seats service add entity graph to seats rep to avoid n+1 problem

--- src/main/java/com/shimady/ticketbookingapp/repository/SeatsRepository.java
+++ src/main/java/com/shimady/ticketbookingapp/repository/SeatsRepository.java
@@ -2,11 +2,13 @@ package com.shimady.ticketbookingapp.repository;
 
 import com.shimady.ticketbookingapp.model.Seat;
 import com.shimady.ticketbookingapp.model.SeatType;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.repository.Repository;
 
 import java.util.List;
 
 public interface SeatsRepository extends Repository<Seat, Long> {
 
+    @EntityGraph(type = EntityGraph.EntityGraphType.LOAD, attributePaths = {"booking"})
     List<Seat> findAllByFlightIdAndType(Long flightId, SeatType seatType);
 }
--- src/main/java/com/shimady/ticketbookingapp/service/SeatsService.java
+++ src/main/java/com/shimady/ticketbookingapp/service/SeatsService.java
@@ -1,12 +1,12 @@
 package com.shimady.ticketbookingapp.service;
 
-import com.shimady.ticketbookingapp.model.Flight;
 import com.shimady.ticketbookingapp.model.Seat;
 import com.shimady.ticketbookingapp.model.SeatType;
 import com.shimady.ticketbookingapp.repository.FlightRepository;
 import com.shimady.ticketbookingapp.repository.SeatsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -20,6 +20,7 @@ public class SeatsService {
         this.seatsRepository = seatsRepository;
     }
 
+    @Transactional(readOnly = true)
     public List<Seat> getSeatsByFlightIdAndType(Long flightId, SeatType seatType) {
         //proper exceptions and handling for them will be added soon
         return seatsRepository.findAllByFlightIdAndType(flightId, seatType);
