177d7c509c466198d12d2c02e9e77e4620a162b3
Maksim Shiryaev <shimady563@gmail.com>
2024-04-21 17:57:55 +0300

    refactor(tickets): refactor some classes convert seats and tickets responses to records make seats repo extend crud repo for usages in future updates

--- src/main/java/com/shimady/ticketbookingapp/repository/SeatsRepository.java
+++ src/main/java/com/shimady/ticketbookingapp/repository/SeatsRepository.java
@@ -3,11 +3,11 @@ package com.shimady.ticketbookingapp.repository;
 import com.shimady.ticketbookingapp.model.Seat;
 import com.shimady.ticketbookingapp.model.SeatType;
 import org.springframework.data.jpa.repository.EntityGraph;
-import org.springframework.data.repository.Repository;
+import org.springframework.data.repository.ListCrudRepository;
 
 import java.util.List;
 
-public interface SeatsRepository extends Repository<Seat, Long> {
+public interface SeatsRepository extends ListCrudRepository<Seat, Long> {
 
     @EntityGraph(type = EntityGraph.EntityGraphType.LOAD, attributePaths = {"booking"})
     List<Seat> findAllByFlightIdAndType(Long flightId, SeatType seatType);
--- src/main/java/com/shimady/ticketbookingapp/service/SeatsService.java
+++ src/main/java/com/shimady/ticketbookingapp/service/SeatsService.java
@@ -2,7 +2,6 @@ package com.shimady.ticketbookingapp.service;
 
 import com.shimady.ticketbookingapp.model.Seat;
 import com.shimady.ticketbookingapp.model.SeatType;
-import com.shimady.ticketbookingapp.repository.FlightRepository;
 import com.shimady.ticketbookingapp.repository.SeatsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -16,13 +15,17 @@ public class SeatsService {
     private final SeatsRepository seatsRepository;
 
     @Autowired
-    public SeatsService(SeatsRepository seatsRepository, FlightRepository flightRepository) {
+    public SeatsService(SeatsRepository seatsRepository) {
         this.seatsRepository = seatsRepository;
     }
 
     @Transactional(readOnly = true)
     public List<Seat> getSeatsByFlightIdAndType(Long flightId, SeatType seatType) {
-        //proper exceptions and handling for them will be added soon
         return seatsRepository.findAllByFlightIdAndType(flightId, seatType);
+    }
+
+    @Transactional(readOnly = true)
+    public List<Seat> getAllSeatsByIds(List<Long> seatIds) {
+        return seatsRepository.findAllById(seatIds);
     }
 }
