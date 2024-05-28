a9c0bb36c8f0591903a5174fcf03d897cf7d7df2
Maksim Shiryaev <shimady563@gmail.com>
2024-05-28 22:46:10 +0300

    feat(validation): add validation constraints to dto

--- src/main/java/com/shimady/ticketbookingapp/controller/dto/BookingRequest.java
+++ src/main/java/com/shimady/ticketbookingapp/controller/dto/BookingRequest.java
@@ -1,12 +1,15 @@
 package com.shimady.ticketbookingapp.controller.dto;
 
 import com.shimady.ticketbookingapp.model.Passenger;
+import jakarta.validation.constraints.NotEmpty;
 
 import java.util.List;
 import java.util.Set;
 
 public record BookingRequest(
+        @NotEmpty(message = "id list cannot be empty")
         List<Long> seatIds,
+        @NotEmpty(message = "passenger list cannot be empty")
         Set<Passenger> passengers
 ) {
 }
--- src/main/java/com/shimady/ticketbookingapp/controller/dto/UserInfo.java
+++ src/main/java/com/shimady/ticketbookingapp/controller/dto/UserInfo.java
@@ -1,8 +1,19 @@
 package com.shimady.ticketbookingapp.controller.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public record UserInfo(
         Long id,
+
+        @NotBlank(message = "username cannot be blank")
+        @Size(min = 4, max = 64,
+                message = "username length should be between 4 and 64 characters")
         String username,
+
+        @Email(regexp = "^[a-zA-Z0-9_!#$%&’*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$",
+                message = "wrong format of email")
         String email
 ) {
 }
