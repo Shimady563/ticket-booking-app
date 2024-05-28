995ea8292628fc7e9438154250ff502c8a5b4c94
Maksim Shiryaev <shimady563@gmail.com>
2024-05-28 22:45:34 +0300

    feat(validation): implement custom password validation

---
+++ src/main/java/com/shimady/ticketbookingapp/validation/PasswordValidator.java
@@ -0,0 +1,13 @@
+package com.shimady.ticketbookingapp.validation;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+public class PasswordValidator implements ConstraintValidator<Password, String> {
+
+    @Override
+    public boolean isValid(String password, ConstraintValidatorContext constraintValidatorContext) {
+        return password != null
+                && password.matches("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@#$%^&+=]).{8,}$");
+    }
+}
