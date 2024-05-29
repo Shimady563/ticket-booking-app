c040e4d3c95850826e7fb2157f336c7af4d7e3b4
Maksim Shiryaev <shimady563@gmail.com>
2024-05-29 12:46:26 +0300

    feat(validation): add request params validation

--- src/main/java/com/shimady/ticketbookingapp/controller/TokenController.java
+++ src/main/java/com/shimady/ticketbookingapp/controller/TokenController.java
@@ -1,10 +1,13 @@
 package com.shimady.ticketbookingapp.controller;
 
 import com.shimady.ticketbookingapp.service.TokenService;
+import jakarta.validation.constraints.NotBlank;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+@Validated
 @RestController
 @RequestMapping("/token")
 public class TokenController {
@@ -18,13 +21,13 @@ public class TokenController {
 
     @GetMapping("")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void verifyToken(@RequestParam("token") String tokenBody) {
+    public void verifyToken(@RequestParam("token") @NotBlank String tokenBody) {
         tokenService.verifyToken(tokenBody);
     }
 
     @GetMapping("/update")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void revokeToken(@RequestParam("token") String tokenBody) {
+    public void revokeToken(@RequestParam("token") @NotBlank String tokenBody) {
         tokenService.updateTokenValidity(tokenBody);
     }
 }
